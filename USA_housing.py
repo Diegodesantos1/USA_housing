6f16108c2bb7ad5cac7ec1d927b8031a347debad
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-05-06 09:22:46 +0200

    reestructuración

--- USA_housing.py
+++ USA_housing.py
@@ -12,18 +12,19 @@ class Analisis:
         for i in range(len(datos)):
             diccionario[i] = datos.iloc[i]
         print(diccionario)
-    def graficas():
+    def graficas_listaRooms():
         plt.subplot(1,2,1)
-        plt.bar(lista_Bedrooms, lista_Income, color = "orange") ; plt.ylabel("Habitaciones") ; plt.xlabel("Sueldo")
+        plt.bar(lista_Rooms, lista_Income, color = "orange") ; plt.ylabel("Habitaciones") ; plt.xlabel("Sueldo")
         plt.subplot(1,2,2)
-        plt.bar(lista_Bedrooms, lista_Age, color = "blue") ; plt.ylabel("Habitaciones") ; plt.xlabel("Edad de la casa")
+        plt.bar(lista_Rooms, lista_Age, color = "blue") ; plt.ylabel("Habitaciones") ; plt.xlabel("Edad de la casa")
         plt.show()
         plt.subplot(1,3,1)
-        plt.bar(lista_Bedrooms, lista_Rooms, color = "red") ; plt.ylabel("Habitaciones") ; plt.xlabel("Casas")
+        plt.bar(lista_Rooms, lista_Bedrooms, color = "red") ; plt.ylabel("Habitaciones") ; plt.xlabel("Casas")
         plt.subplot(1,3,2)
-        plt.bar(lista_Bedrooms, lista_Population, color = "yellow") ; plt.ylabel("Habitaciones") ; plt.xlabel("Población")
+        plt.bar(lista_Rooms, lista_Population, color = "yellow") ; plt.ylabel("Habitaciones") ; plt.xlabel("Población")
         plt.subplot(1,3,3)
-        plt.bar(lista_Bedrooms, lista_Price, color = "purple") ; plt.ylabel("Habitaciones") ; plt.xlabel("Precio")
+        plt.bar(lista_Rooms, lista_Price, color = "purple") ; plt.ylabel("Habitaciones") ; plt.xlabel("Precio")
         plt.show()
 Analisis.crear_diccionario()
-Analisis.graficas()
+Analisis.graficas_listaRooms()
+Analisis.graficas_listaPrice()
