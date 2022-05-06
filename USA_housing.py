7e38863c26b8a249912620e53a94eab0fe0a89e4
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-05-06 08:59:27 +0200

    planteado el ejercicio

--- USA_housing.py
+++ USA_housing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-datos = pd.read_csv("Housing", sep= ",")
-print(datos)
-
+datos = pd.read_csv("Housing.csv", sep= ",")
+lista_Income = list(datos["Avg. Area Income"]) ; lista_Age = list(datos["Avg. Area House Age"]) ; lista_Rooms = list(datos["Avg. Area Number of Rooms"])
+lista_Bedrooms = list(datos["Avg. Area Number of Bedrooms"]) ; lista_Population = list(datos["Area Population"]) ; lista_Price = list(datos["Price"]) ; lista_Address = list(datos["Address"])
